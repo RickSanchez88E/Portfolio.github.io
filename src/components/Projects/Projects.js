3abdb07b88e376a6c469ba230eb9e8f15c38c05b
RickSanchez88E <k21078271@kcl.ac.uk>
2024-05-03 11:26:30 +0100

    change the title

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,6 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
 import crimson from "../../Assets/Projects/crimson.png"
 import suicide from "../../Assets/Projects/suicide.png";
 import sudoku from "../../Assets/Projects/sudoku.png";
